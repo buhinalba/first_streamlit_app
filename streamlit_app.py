6eab7a97e4b6860835df26588de6319cb6e21669
buhinalba <62543082+buhinalba@users.noreply.github.com>
2023-03-20 16:48:49 +0100

    Update streamlit_app.py
    fix: update var name

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice')
 
 fruit_search = "kiwi"
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+kiwi)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_search)
 
 
 # normalize json response
