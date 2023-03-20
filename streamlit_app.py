5f142c3b0f59bd27d2b1b5c00e92f1801c3c5bde
buhinalba <62543082+buhinalba@users.noreply.github.com>
2023-03-20 10:23:53 +0100

    Update streamlit_app.py
    add emojis before menu items
    add new menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast favorites')
+streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocad Toast')
+
+
