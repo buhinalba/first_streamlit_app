56e56970400fc0a30610bb968e98842ffe22f3bb
buhinalba <62543082+buhinalba@users.noreply.github.com>
2023-03-21 12:13:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,5 +50,5 @@ streamlit.dataframe(my_data_row)
 
 # add input box to be able to extend list
 fruit_to_add = streamlit.text_input('What kind of fruit would you like to add?')
-my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ("+fruit_to_add+")")
+my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('"+fruit_to_add+"')")
 
